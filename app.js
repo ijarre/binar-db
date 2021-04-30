e640a6e2962ae893a38d293b654c22a0f6ba5447
Faizhar Riskisyah <mohamad.faizhar@gmail.com>
2021-04-30 20:43:07 +0700

    fix: linking JS to HTML

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static("src"));
+app.use(express.static(path.join(__dirname, "src")));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./src/landing-page/index.html"));
 });
 app.get("/suwit", (req, res) => {
   res.sendFile(path.join(__dirname, "./src/page-component-suwit/index.html"));
+});
+app.get("/script.js", (req, res) => {
+  res.sendFile(path.join(__dirname, "./src/page-component-suwit/script.js"));
+});
+app.get("/game.js", (req, res) => {
+  res.sendFile(path.join(__dirname, "./src/page-component-suwit/game.js"));
 });
 
 app.listen(port);
