95dbd88620a440b897a0a4ada80f4a7862b789b2
Faizhar Riskisyah <mohamad.faizhar@gmail.com>
2021-07-16 08:36:46 +0700

    feat: setting express endpoint

--- app.js
+++ app.js
@@ -1,21 +1,52 @@
 const express = require("express");
-const path = require("path");
-const routes = require("./src/startup/routes");
+
+const user = require("./data/user.json");
 const app = express();
-const bodyParser = require("body-parser");
+
 const port = process.env.PORT || 3000;
-require("dotenv").config();
 
-app.use(express.static(path.join(__dirname, "src")));
-app.use(bodyParser.json());
-app.use(routes);
+app.set("view engine", "ejs");
+app.use(express.static("./public"));
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./src/landing-page/index.html"));
-});
-app.get("/suwit", (req, res) => {
-  res.sendFile(path.join(__dirname, "./src/page-component-suwit/index.html"));
+app.get("/", (req, res) => res.status(200).render("main"));
+app.get("/suwit", (req, res) => res.status(200).render("suwit"));
+app.get("/login", (req, res) => {
+  res.status(200).render("login");
 });
 
-app.listen(port);
-console.log("Server started at http://localhost:" + port);
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  if (
+    user.filter((el) => el.username === username && el.password === password)
+      .length !== 0
+  ) {
+    alert(
+      `a new user just logged in by username ${username} and password ${password}`
+    );
+    res.redirect("/");
+  } else {
+    throw new Error("user tidak ditemukan");
+  }
+});
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({
+    status: "fail",
+    errors: err.message,
+  });
+  next();
+});
+
+app.use((req, res, next) => {
+  res.status(404).json({
+    status: "fail",
+    errors: "Page not found",
+  });
+  next();
+});
+
+app.listen(port, () =>
+  console.log("Server started at http://localhost:" + port)
+);
